1bf2b1b6bbedfcbef29ea37b0f9fed6d119e2c27
tobi-hana <eltishev2@gmail.com>
2023-10-31 21:37:50 +0300

    correct task1

--- task1/first-task/src/main/java/ru/perfomancelab/Main1.java
+++ task1/first-task/src/main/java/ru/perfomancelab/Main1.java
@@ -1,6 +1,7 @@
-package ru.perfomancelab.task1;
+package ru.perfomancelab;
 
-public class MainApp1 {
+
+public class Main1 {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int m = Integer.parseInt(args[1]);
